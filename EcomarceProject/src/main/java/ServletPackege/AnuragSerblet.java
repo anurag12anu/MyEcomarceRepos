e7fbf577f10d8aef8c7e63fda2e6b61ecf9beb3b
Hello <Hello@DESKTOP-J33EPHV>
2024-02-08 23:05:17 -0800

    AnuragSer commit

---
+++ EcomarceProject/src/main/java/ServletPackege/AnuragSerblet.java
@@ -0,0 +1,33 @@
+package ServletPackege;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AnuragSerblet
+ */
+@WebServlet("/AnuragSerblet")
+public class AnuragSerblet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AnuragSerblet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
